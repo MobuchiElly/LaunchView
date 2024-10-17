e622f1415fc8dde0ddf5849003a91ec87145648f
MobuchiElly <ugwueleazer@gmail.com>
2024-10-17 05:41:27 +0100

    fixed typescript-eslint errors

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
+import {redirect} from 'next/navigation';
 
 const page = () => {
+  const workingOnPage = false;
+  if(!workingOnPage){
+    redirect('/projectView');
+  }
   return (
     <div className='text-center'>Welcome Page</div>
   )
